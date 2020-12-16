98eb703530a063ee215adaf8c001a8da462a0562
Hubtwork <hubtwork@gmail.com>
2020-12-17 08:55:54 +0900

    s

---
+++ src/main/kotlin/com/hubtwork/lola/LolaApplication.kt
@@ -0,0 +1,11 @@
+package com.hubtwork.lola
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LolaApplication
+
+fun main(args: Array<String>) {
+    runApplication<LolaApplication>(*args)
+}
---
+++ src/test/kotlin/com/hubtwork/lola/LolaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hubtwork.lola
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LolaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

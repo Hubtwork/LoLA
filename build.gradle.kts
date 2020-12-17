b9135ba4215af975b7977b4349106fba406a7371
Hubtwork <hubtwork@gmail.com>
2020-12-17 23:22:49 +0900

    RIOT_API_Setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // dependency for WebFlux libraries... like webClient
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
